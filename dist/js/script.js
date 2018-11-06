87d31e84e50834aee6d98ce5a4892e0489aa9c2d
Kirill Fyodorov <kirill-fjodorov00000@yandex.ru>
2018-11-06 14:46:44 +0300

    Добавил скролл страницы

--- dist/js/script.js
+++ dist/js/script.js
@@ -464,10 +464,9 @@ function forms() {
             clearInput();
 
             if (form.className.indexOf('popup') != -1) {
-              console.log(1);
               var popupClass = document.querySelector('.' + form.className.slice(0, -5));
-              console.log(1);
               popupClass.style.display = 'none';
+              document.querySelector('body').style.overflow = '';
             }
 
             clearInterval(closeFormTimer);
